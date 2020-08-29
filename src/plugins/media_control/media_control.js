9b43859c722a8e65bdd830114244891d7c96edd8
João Paulo <vieira.joaopaulo.91@gmail.com>
2020-08-28 22:17:17 -0300

    feat(media_control): implement seekbar

--- src/plugins/media_control/media_control.js
+++ src/plugins/media_control/media_control.js
@@ -32,7 +32,21 @@ export default class MediaControlPlugin extends UICorePlugin {
       'click .play-pause-button[data-pause-button]': 'pause',
       'click .rewind-button': 'rewind',
       'click .fast-forward-button': 'fastForward',
+      'click .media-control__seekbar-container': 'seek',
+      'mousedown .media-control__seekbar-scrubber': 'startSeekDrag',
+      'mousemove .media-control__seekbar-container': 'mousemoveOnSeekBar',
+      'mouseleave .media-control__seekbar-container': 'mouseleaveOnSeekBar',
     }
+  }
+
+  constructor(core) {
+    super(core)
+
+    this.stopDragHandler = event => this.stopDrag(event)
+    this.updateDragHandler = event => this.updateDrag(event)
+
+    document.addEventListener('mouseup', this.stopDragHandler)
+    document.addEventListener('mousemove', this.updateDragHandler)
   }
 
   bindEvents() {
@@ -46,6 +60,7 @@ export default class MediaControlPlugin extends UICorePlugin {
     this.listenTo(this.container, Events.CONTAINER_PLAY, this.changeTogglePlay)
     this.listenTo(this.container, Events.CONTAINER_PAUSE, this.changeTogglePlay)
     this.listenTo(this.container, Events.CONTAINER_TIMEUPDATE, this.onTimeUpdate)
+    this.listenTo(this.container, Events.CONTAINER_PROGRESS, this.updateProgressBar)
   }
 
   onActiveContainerChanged() {
@@ -57,6 +72,13 @@ export default class MediaControlPlugin extends UICorePlugin {
 
   setInitialState() {
     this.changeTogglePlay()
+
+    let previousSeekPercentage = 0
+    if (this.displayedSeekBarPercentage)
+      previousSeekPercentage = this.displayedSeekBarPercentage
+
+    this.displayedSeekBarPercentage = null
+    this.setSeekPercentage(previousSeekPercentage)
   }
 
   play() {
@@ -68,6 +90,8 @@ export default class MediaControlPlugin extends UICorePlugin {
   }
 
   onTimeUpdate(timeProgress) {
+    if (this.draggingSeekBar) return
+
     this.currentPositionValue = timeProgress.current < 0 ? timeProgress.total : timeProgress.current
 
     if (this.currentDurationValue !== timeProgress.total) {
@@ -81,6 +105,8 @@ export default class MediaControlPlugin extends UICorePlugin {
   updatePosition() {
     const formattedCurrentPosition = Utils.formatTime(this.currentPositionValue)
     this.$position.innerText = formattedCurrentPosition
+
+    this.renderSeekBar()
   }
 
   updateDuration() {
@@ -117,10 +143,19 @@ export default class MediaControlPlugin extends UICorePlugin {
 
   destroy() {
     this.isRendered = false
+
+    document.removeEventListener('mouseup', this.stopDragHandler)
+    document.removeEventListener('mousemove', this.updateDragHandler)
     super.destroy()
   }
 
   cacheElements() {
+    this.$seekBarContainer = this.el.querySelector('.media-control__seekbar-container')
+    this.$seekBarHover = this.el.querySelector('.media-control__seekbar-hover')
+    this.$seekBarLoaded = this.el.querySelector('.media-control__seekbar-fill-1')
+    this.$seekBarPosition = this.el.querySelector('.media-control__seekbar-fill-2')
+    this.$seekBarScrubber = this.el.querySelector('.media-control__seekbar-scrubber')
+
     this.$buttonsContainer = this.el.querySelector('.media-control__buttons-container')
     this.$timersContainer = this.el.querySelector('.media-control__timers-container')
 
@@ -145,7 +180,95 @@ export default class MediaControlPlugin extends UICorePlugin {
 
     this.$el.html(this.template())
     this.cacheElements()
-    this.setInitialState()
+    window.setTimeout(() => this.setInitialState(), 100)
     this.core.$el.append(this.el)
+  }
+
+  mousemoveOnSeekBar(event) {
+    const offsetX = event.pageX - this.$seekBarContainer.getBoundingClientRect().left - (this.$seekBarHover.style.width / 2)
+    this.$seekBarHover.style.cssText = `left: ${offsetX}`
+    this.trigger(Events.MEDIACONTROL_MOUSEMOVE_SEEKBAR, event)
+  }
+
+  mouseleaveOnSeekBar(event) {
+    this.trigger(Events.MEDIACONTROL_MOUSELEAVE_SEEKBAR, event)
+  }
+
+  startSeekDrag(event) {
+    this.draggingSeekBar = true
+    this.$seekBarLoaded.classList.add('media-control--no-transition')
+    this.$seekBarPosition.classList.add('media-control--no-transition')
+    this.$seekBarScrubber.classList.add('media-control--no-transition')
+    event && event.preventDefault()
+  }
+
+  stopDrag(event) {
+    this.draggingSeekBar && this.seek(event)
+    this.$seekBarLoaded.classList.remove('media-control--no-transition')
+    this.$seekBarPosition.classList.remove('media-control--no-transition')
+    this.$seekBarScrubber.classList.remove('media-control--no-transition')
+    this.draggingSeekBar = false
+    this.draggingVolumeBar = false
+  }
+
+  updateDrag(event) {
+    if (!this.draggingSeekBar) return
+
+    event.preventDefault()
+    const offsetX = event.pageX - this.$seekBarContainer.getBoundingClientRect().left
+    let pos = offsetX / this.$seekBarContainer.getBoundingClientRect().width * 100
+    pos = Math.min(100, Math.max(pos, 0))
+    this.setSeekPercentage(pos)
+  }
+
+  updateProgressBar(progress) {
+    const loadedStart = progress.start / progress.total * 100
+    const loadedEnd = progress.current / progress.total * 100
+    this.$seekBarLoaded.style.cssText = `left: ${loadedStart}%; width: ${loadedEnd - loadedStart}%;`
+  }
+
+  renderSeekBar() {
+    // this will be triggered as soon as these become available
+    if (this.currentPositionValue === null || this.currentDurationValue === null) return
+
+    // default to 100%
+    this.currentSeekBarPercentage = 100
+
+    if (this.container)
+      this.currentSeekBarPercentage = (this.currentPositionValue / this.currentDurationValue) * 100
+
+    this.setSeekPercentage(this.currentSeekBarPercentage)
+
+    const newPosition = Utils.formatTime(this.currentPositionValue)
+    const newDuration = Utils.formatTime(this.currentDurationValue)
+    if (newPosition !== this.displayedPosition) {
+      this.$position.innerText = newPosition
+      this.displayedPosition = newPosition
+    }
+    if (newDuration !== this.displayedDuration) {
+      this.$duration.innerText = newDuration
+      this.displayedDuration = newDuration
+    }
+  }
+
+  seek(event) {
+    const offsetX = event.pageX - this.$seekBarContainer.getBoundingClientRect().left
+    let pos = offsetX / this.$seekBarContainer.getBoundingClientRect().width * 100
+    pos = Math.min(100, Math.max(pos, 0))
+    this.container && this.container.seekPercentage(pos)
+    this.setSeekPercentage(pos)
+    return false
+  }
+
+  setSeekPercentage(value) {
+    const percentageValue = Math.max(Math.min(value, 100.0), 0)
+    // not changed since last update
+    if (this.displayedSeekBarPercentage === percentageValue) return
+
+    this.displayedSeekBarPercentage = percentageValue
+    this.$seekBarPosition.classList.remove('media-control--no-transition')
+    this.$seekBarScrubber.classList.remove('media-control--no-transition')
+    this.$seekBarPosition.style.cssText = `width: ${percentageValue}%`
+    this.$seekBarScrubber.style.cssText = `left: ${percentageValue}%`
   }
 }
